d7a9fb8944536a79b4dc8106319a100ed31ef4da
Samiyar Teimourian <68439377+samiyar-teimourian@users.noreply.github.com>
2026-08-27 15:44:02 +0330

    Correct Push

--- config/urls.py
+++ config/urls.py
@@ -18,13 +18,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from shop.views import prod , products
+from shop.views import *
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("products/<int:id>/", prod, name="product"),
-    path("products/", products, name="product"),
+    path("products/", ProductListView.as_view()),
+    path("products/<int:id>/", ProductDetailView.as_view()),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
