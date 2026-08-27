1b5db131fa344946649da3e3555c7afa5f823fba
Samiyar Teimourian <68439377+samiyar-teimourian@users.noreply.github.com>
2026-08-27 15:42:53 +0330

    Correct Push

--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,14 @@
-from django.shortcuts import get_object_or_404
-from .models import product
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Product
+from .serializers import ProductSerializer
 
-@api_view(["GET"])
-def prod(request ,id):
-    productt = get_object_or_404(product, id=id)
-    return Response({
-        "id": productt.id,
-        "name": productt.name,
-        "price": productt.price,
-    })
-@api_view(["GET"])
-def products(request):
-    product_list = list(product.objects.all().values())
-    return Response({"productlist": product_list})
+
+class ProductListView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "id"
